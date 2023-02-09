2660fe6ab9c74f773edddd4c00bccf7fcf9d8b95
Gustavo Guichard <gustavoguichard@gmail.com>
2023-02-08 22:39:19 -0300

    DRY error and catch boundaries in root.tsx

--- app/routes/difficulty.tsx
+++ app/routes/difficulty.tsx
@@ -1,11 +1,5 @@
-import {
-  Form,
-  useTransition,
-  useActionData,
-  Link,
-  useCatch,
-} from '@remix-run/react'
-import type { ActionArgs, ErrorBoundaryComponent } from '@remix-run/node'
+import { Form, useTransition, useActionData } from '@remix-run/react'
+import type { ActionArgs } from '@remix-run/node'
 import { redirect } from '@remix-run/node'
 import badRequest from '~/utils/badRequest'
 import { getUserSession, storage } from '~/utils/session.server'
@@ -162,43 +156,5 @@ export default function Difficulty() {
         </Form>
       </div>
     </main>
-  )
-}
-
-export function CatchBoundary() {
-  const caught = useCatch()
-  return (
-    <div className="grid h-screen w-full place-items-center bg-black bg-opacity-50 bg-[url('https://i.pinimg.com/originals/10/c7/ba/10c7badbea3bcd027b202f6134f8020c.jpg')] bg-cover bg-center bg-no-repeat bg-blend-overlay">
-      <div className="text-white">
-        <h1 className="text-4xl font-bold">Oops!!</h1>
-        <p>Status: {caught.status}</p>
-        <pre>
-          <code>{caught.data}</code>
-        </pre>
-        <div className="mt-4 flex justify-center">
-          <Link to="/difficulty" className="bg-white px-6 py-3 text-black">
-            Try again
-          </Link>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-export const ErrorBoundary: ErrorBoundaryComponent = ({ error }) => {
-  console.log({ error: error.message })
-  return (
-    <div className="grid h-screen w-full place-items-center bg-black bg-opacity-40 bg-[url('https://media1.popsugar-assets.com/files/thumbor/hD4DY5UeYUO_rmi7BbQw05P03vw/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/05/19/288/n/1922283/3c59feec5ce2412a2a2935.47224303__6_Courtesy_of_HBO/i/Why-Daenerys-Targaryen-Death-So-Damn-LAME.jpg')] bg-cover bg-center bg-no-repeat text-white bg-blend-overlay">
-      <div>
-        <h1 className="text-center text-4xl font-bold">
-          Oops!! Something's not right
-        </h1>
-        <div className="mt-4 flex justify-center">
-          <Link to="/difficulty" className="bg-white px-6 py-3 text-black">
-            Try again
-          </Link>
-        </div>
-      </div>
-    </div>
   )
 }
